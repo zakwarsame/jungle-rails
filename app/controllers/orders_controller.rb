7863f4a4a993c7a0e3f897c56f54034330c3df21
Zak Warsame <zakwarsamee@gmail.com>
2021-10-13 19:53:07 +0000

    add line items to show when an order has been successfully placed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,8 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @line_items = @order.line_items
+    # @products = Product.find @line_items[:product_id]
   end
 
   def create
